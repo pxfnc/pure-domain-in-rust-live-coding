f504120f6b1e851336306a2d019e12680233e440
pxfnc <41906308+pxfnc@users.noreply.github.com>
2024-01-28 22:31:28 +0900

    chotto god

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use std::collections::HashMap;
 
-// #[derive(Debug)]
-// struct User {
-//     id: u64,
-//     name: String
-// }
+#[derive(Debug)]
+struct User {
+    id: u64,
+    name: String,
+}
+
+impl User {
+    pub fn new(id: u64, name: String) -> Result<Self, &'static str> {
+        if name.len() > 6 {
+            Err("5バイト以下")
+        } else {
+            Ok(Self { id, name })
+        }
+    }
+}
 
 type DB = HashMap<u64, HashMap<&'static str, String>>;
 
@@ -22,9 +32,20 @@ fn create_user_usecase_bad(db: &mut DB, name: String) -> Result<(), &'static str
     Ok(())
 }
 
+fn create_user_usecase_chotto_god(db: &mut DB, name: String) -> Result<(), &'static str> {
+    let new_id = db.keys().max().map(|n| n + 1).unwrap_or_default();
+
+    let user = User::new(new_id, name)?;
+    let mut record = HashMap::new();
+    record.insert("id", user.id.to_string());
+    record.insert("name", user.name);
+    db.insert(new_id, record);
+    Ok(())
+}
+
 fn main() {
     let mut db: DB = HashMap::new();
-    let _ = create_user_usecase_bad(&mut db, "Taro".to_string());
-    let _ = create_user_usecase_bad(&mut db, "Jiro".to_string());
+    let _ = create_user_usecase_chotto_god(&mut db, "Taro".to_string());
+    let _ = create_user_usecase_chotto_god(&mut db, "Jiro".to_string());
     println!("{:?}", db);
 }
